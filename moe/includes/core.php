750d3c2cc1916bdd0be5b029bfced0955eed20d3
Neku <neku@pomf.se>
2013-12-17 02:15:17 +0100

    Update core.php

--- moe/includes/core.php
+++ moe/includes/core.php
@@ -1,5 +1,7 @@
 <?php
 // This file will handle all functions used by Pomf's Moe Panel
+// and just like the rest of the moe code this is a mess and will be
+// replaced soon
 session_start();
 $db = new PDO('mysql:unix_socket=/var/run/mysqld/mysqld.sock;dbname=xxx', 'xxx', 'xxx', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"));
 
