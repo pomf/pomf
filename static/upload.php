7a801291476b0399da262ecec65315fbde17c95f
Neku <neku@pomf.se>
2014-02-22 20:17:13 +0100

    Update upload.php

--- static/upload.php
+++ static/upload.php
@@ -1,6 +1,7 @@
 <?php
 //error_reporting(E_ERROR | E_WARNING | E_PARSE);
 include_once 'classes/UploadedFile.class.php';
+//include_once 'includes/database.inc.php'; Included in database.inc.php
 include_once 'includes/database.inc.php';
 /**
  * Generates a name for the file, retrying until we get an unused one 
