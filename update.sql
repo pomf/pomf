aa61832a73bac8e1045b70a887e5bee1839599d1
alucard0134 <austi_gillm935@ahapps.anoka.k12.mn.us>
2015-06-18 14:58:21 -0500

    Spelling error of the century

--- update.sql
+++ update.sql
@@ -3,7 +3,7 @@ CHANGE COLUMN `id` `id` int(10) unsigned NOT NULL auto_increment;
 
 ALTER TABLE `files` 
 CHANGE COLUMN `id` `id` int(10) unsigned NOT NULL auto_increment,
-CHANGE COLUMN `orginalname` `originalname` varchar(255) NULL default NULL,
+CHANGE COLUMN `originalname` `originalname` varchar(255) NULL default NULL,
 CHANGE COLUMN `size` `size` int(10) NULL default NULL,
 CHANGE COLUMN `user` `user` int(10) NULL default '0';
 
