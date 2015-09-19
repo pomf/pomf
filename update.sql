f75694659f5f8438c265709c799750dfcff8cbcc
WubTheCaptain <wub@partyvan.eu>
2015-09-19 17:06:50 +0000

    Give update.sql some dos2unix(1) treatment

--- update.sql
+++ update.sql
@@ -1,16 +1,16 @@
-ALTER TABLE `accounts` 
+ALTER TABLE `accounts`
 CHANGE COLUMN `id` `id` int(10) unsigned NOT NULL auto_increment;
 
-ALTER TABLE `files` 
+ALTER TABLE `files`
 CHANGE COLUMN `id` `id` int(10) unsigned NOT NULL auto_increment,
 CHANGE COLUMN `orginalname` `originalname` varchar(255) NULL default NULL,
 CHANGE COLUMN `size` `size` int(10) NULL default NULL,
 CHANGE COLUMN `user` `user` int(10) NULL default '0';
 
-ALTER TABLE `invites` 
+ALTER TABLE `invites`
 CHANGE COLUMN `id` `id` int(10) unsigned NOT NULL auto_increment;
 
-ALTER TABLE `reports` 
+ALTER TABLE `reports`
 CHANGE COLUMN `id` `id` int(10) unsigned NOT NULL auto_increment,
 CHANGE COLUMN `file` `file` varchar(255) NOT NULL,
 CHANGE COLUMN `reporter` `reporter` varchar(255) NOT NULL;
