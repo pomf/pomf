e7a8961a1674477f64048d995dfad9dcc3b15fce
Juuso Lapinlampi <wub@partyvan.eu>
2015-09-27 07:37:14 +0000

    Add Pantsu.cat copyright to js/pomf.js
    This compliments commit 7dca38c.

--- js/pomf.js
+++ js/pomf.js
@@ -1,5 +1,6 @@
 /**
  * Copyright (c) 2013 Peter Lejeck <peter.lejeck@gmail.com>
+ * Copyright (c) 2015 the Pantsu.cat developers <hostmaster@pantsu.cat>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
