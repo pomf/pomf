pomf/pomf

commit 2ca5b6b743487477fdaf045a7d4abb605780a300

Sort upload responses alphabetically
This commit improves readability by swapping text and html response
source line order for alphabetical sorting.

static/classes/Response.class.php
@@ -37,12 +37,12 @@ class Response
             case 'gyazo':
                 // Deprecated API since version 2.0.0
                 // fallthrough
-            case 'text':
-                header('Content-Type: text/plain; charset=UTF-8');
-                $this->type = $response_type;
-                break;
             case 'html':
                 header('Content-Type: text/html; charset=UTF-8');
+                $this->type = $response_type;
+                break;
+            case 'text':
+                header('Content-Type: text/plain; charset=UTF-8');
                 $this->type = $response_type;
                 break;
             default:
@@ -60,11 +60,11 @@ class Response
             case 'csv':
                 $response = $this->csv_error($desc);
                 break;
-            case 'text':
-                $response = $this->text_error($code, $desc);
-                break;
             case 'html':
                 $response = $this->html_error($code, $desc);
+                break;
+            case 'text':
+                $response = $this->text_error($code, $desc);
                 break;
             default:
                 $response = $this->json_error($code, $desc);
@@ -83,11 +83,11 @@ class Response
             case 'csv':
                 $response = $this->csv_success($files);
                 break;
-            case 'text':
-                $response = $this->text_success($files);
-                break;
             case 'html':
                 $response = $this->html_success($files);
+                break;
+            case 'text':
+                $response = $this->text_success($files);
                 break;
             default:
                 $response = $this->json_success($files);
@@ -116,20 +116,6 @@ class Response
         return $result;
     }
 
-    private static function text_error($code, $description)
-    {
-        return 'ERROR: ('.$code.') '.$description;
-    }
-
-    private static function text_success($files)
-    {
-        foreach ($files as $file) {
-            $result .= $file['url']."\n";
-        }
-
-        return $result;
-    }
-
     private static function html_error($code, $description)
     {
         return '<p>ERROR: ('.$code.') '.$description.'</p>';
@@ -139,6 +125,20 @@ class Response
     {
         foreach ($files as $file) {
             $result .=  '<a href="'.$file['url'].'">'.$file['url'].'</a><br>';
+        }
+
+        return $result;
+    }
+
+    private static function text_error($code, $description)
+    {
+        return 'ERROR: ('.$code.') '.$description;
+    }
+
+    private static function text_success($files)
+    {
+        foreach ($files as $file) {
+            $result .= $file['url']."\n";
         }
 
         return $result;

commit daac77a1962d8f256b3e7c5a0b46c10c833f688a

Fix Gyazo response falling through back to text again
This fixes a silly mistake where Gyazo would accidentally use the HTML
response instead of text as intended.

This bug was introduced in commit 2ca5b6b.

static/classes/Response.class.php
@@ -34,9 +34,6 @@ class Response
                 header('Content-Type: text/csv; charset=UTF-8');
                 $this->type = $response_type;
                 break;
-            case 'gyazo':
-                // Deprecated API since version 2.0.0
-                // fallthrough
             case 'html':
                 header('Content-Type: text/html; charset=UTF-8');
                 $this->type = $response_type;
@@ -45,6 +42,9 @@ class Response
                 header('Content-Type: application/json; charset=UTF-8');
                 $this->type = $response_type;
                 break;
+            case 'gyazo':
+                // Deprecated API since version 2.0.0
+                // fallthrough
             case 'text':
                 header('Content-Type: text/plain; charset=UTF-8');
                 $this->type = $response_type;