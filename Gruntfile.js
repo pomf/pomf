pomf/pomf

commit 90e6b7bdfeccff5e96f0bda23bc5c9ab7b33238a

Remove grunt-contrib-imagemin image minification
Ever since liberating images in c76f70c, there's no really a need to
minify images. Furthermore, the dependency update in 6ef710a changed PNG
minification to be lossless, so most of the benefits were lost (because
our images are already compressed in the repository).

We don't need this anymore.

Gruntfile.js
@@ -76,16 +76,6 @@ module.exports = function (grunt) {
         create: ['dist/img']
       }
     },
-    imagemin: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: 'img/',
-          src: '**/*.{png,jpg,gif}',
-          dest: 'dist/img/'
-        }]
-      }
-    },
     copy: {
       dist: {
         files: [{
@@ -103,8 +93,7 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-htmlmin');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-contrib-copy');
 
-  grunt.registerTask('default', ['mkdir', 'swig', 'htmlmin', 'cssmin', 'uglify', 'imagemin', 'copy']);
+  grunt.registerTask('default', ['mkdir', 'swig', 'htmlmin', 'cssmin', 'uglify', 'copy']);
 };

commit c2af70b9e5b53505a37bfd144ac5fff6d2af05a8

Add img/ to Gruntfile copy task
Grunt would not copy `img/` to `dist/img/`, because the task of copying
images was previously on the now-removed imagemin task.

This bug was introduced in commit 90e6b7b.

Gruntfile.js
@@ -68,6 +68,12 @@ module.exports = function (grunt) {
           cwd: 'static/',
           src: '**',
           dest: 'dist/'
+        },
+        {
+          expand: true,
+          cwd: 'img/',
+          src: '**',
+          dest: 'dist/img/'
         }]
       }
     }