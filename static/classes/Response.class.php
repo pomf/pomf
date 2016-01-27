pomf/pomf

commit a92961f24072bc4c3881bf0e2f609562f3703471

Add text response class
This allows for text only output which removes
the complexity of many bash uploader scripts.

static/classes/Response.class.php
@@ -12,6 +12,11 @@ class Response
                 header('Content-Type: text/plain; charset=UTF-8');
                 $this->type = $response_type;
                 break;
+            case 'text':
+                header('Content-Type: text/plain; charset=UTF-8');
+                $this->type = $response_type;
+                break;
+
             case 'html': 
                 header('Content-Type: text/html;charset=utf-8');
                 $this->type = 'html';
@@ -33,6 +38,9 @@ class Response
                 break;
             case 'gyazo':
                 $response = $this->gyazo_error($code, $desc);
+                break;
+            case 'text':
+                $response = $this->text_error($code, $desc);
                 break;
             case 'html':
                 $response = $this->html_error($code, $desc);
@@ -59,6 +67,9 @@ class Response
                 break;
             case 'gyazo':
                 $response = $this->gyazo_success($files);
+                break;
+            case 'text':
+                $response = $this->text_success($files);
                 break;
             default:
                 $response = $this->json_success($files);
@@ -96,6 +107,20 @@ class Response
     {
         return POMF_URL.$files[0]['url'];
     }
+    private static function txt_error($code, $description)
+    {
+        return 'ERROR: ('.$code.') '.$description;
+    }
+
+    private static function text_success($files)
+    {
+        foreach ($files as $file) {
+            $result .=  POMF_URL.$files[0]['url'];
+        }
+
+    return $result;
+    }
+
     private static function html_error($code, $description)
     {
         return 'ERROR: ('.$code.') '.$description;

commit 5ac30eb86d2c5ac5b4e72651575600f46c2c8a01

Fix undeclared vars in html_success() and text_success()
As explained by clsr:

> <clsr> With default PHP settings, that'd print a warning, then treat
> $result as if it was an empty string before appending.

This bug was introduced in commit
a92961f24072bc4c3881bf0e2f609562f3703471 and
1411bce1dc13bf6fea66b5899ba29d50d5c53bb0.

Reported-by: clsr <clsr@clsr.net>
Suggested-by: clsr <clsr@clsr.net>

static/classes/Response.class.php
@@ -130,6 +130,8 @@ class Response
 
     private static function html_success($files)
     {
+        $result = '';
+
         foreach ($files as $file) {
             $result .=  '<a href="'.$file['url'].'">'.$file['url'].'</a><br>';
         }
@@ -161,6 +163,8 @@ class Response
 
     private static function text_success($files)
     {
+        $result = '';
+
         foreach ($files as $file) {
             $result .= $file['url']."\n";
         }