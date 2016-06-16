edcef8b05604c30e0f3e6273fc874dfc860e3d72
Eliot Whalan <ewhal@pantsu.cat>
2016-06-16 16:25:05 +1000

    Add DOMContentLoaded so js will only be executed on load

--- js/app.js
+++ js/app.js
@@ -1,149 +1,152 @@
-function addRow(file) {
-  var row = document.createElement('li');
+document.addEventListener('DOMContentLoaded', function() {
 
-  var name = document.createElement('span');
-  name.textContent = file.name;
-  name.className = 'file-name';
+  function addRow(file) {
+    var row = document.createElement('li');
 
-  var progressIndicator = document.createElement('span');
-  progressIndicator.className = 'progress-percent';
-  progressIndicator.textContent = '0%';
+    var name = document.createElement('span');
+    name.textContent = file.name;
+    name.className = 'file-name';
 
-  var progressBar = document.createElement('progress');
-  progressBar.className = 'file-progress';
-  progressBar.setAttribute("max", "100");
-  progressBar.setAttribute("value", "0");
+    var progressIndicator = document.createElement('span');
+    progressIndicator.className = 'progress-percent';
+    progressIndicator.textContent = '0%';
 
-  row.appendChild(name);
-  row.appendChild(progressBar);
-  row.appendChild(progressIndicator);
+    var progressBar = document.createElement('progress');
+    progressBar.className = 'file-progress';
+    progressBar.setAttribute("max", "100");
+    progressBar.setAttribute("value", "0");
 
-  document.getElementById('upload-filelist').appendChild(row);
-  return row;
-}
+    row.appendChild(name);
+    row.appendChild(progressBar);
+    row.appendChild(progressIndicator);
 
-function handleUploadProgress(evt) {
-  var xhr = evt.target;
-  var bar = xhr.bar;
-  var percentIndicator = xhr.percent;
-
-  if (evt.lengthComputable) {
-    var progressPercent = Math.floor((evt.loaded / evt.total) * 100);
-    bar.setAttribute("value", progressPercent);
-    percentIndicator.textContent = progressPercent + '%';
+    document.getElementById('upload-filelist').appendChild(row);
+    return row;
   }
-}
 
-function handleUploadComplete(evt) {
-  var xhr = evt.target;
-  var bar = xhr.bar;
-  var row = xhr.row;
-  var percentIndicator = xhr.percent;
+  function handleUploadProgress(evt) {
+    var xhr = evt.target;
+    var bar = xhr.bar;
+    var percentIndicator = xhr.percent;
 
-  percentIndicator.style.visibility = "hidden";
-  bar.style.visibility = "hidden";
-  row.removeChild(bar);
-  row.removeChild(percentIndicator);
-  var respStatus = xhr.status;
+    if (evt.lengthComputable) {
+      var progressPercent = Math.floor((evt.loaded / evt.total) * 100);
+      bar.setAttribute("value", progressPercent);
+      percentIndicator.textContent = progressPercent + '%';
+    }
+  }
 
-  var url = document.createElement('span');
-  url.className = 'file-url';
-  row.appendChild(url);
+  function handleUploadComplete(evt) {
+    var xhr = evt.target;
+    var bar = xhr.bar;
+    var row = xhr.row;
+    var percentIndicator = xhr.percent;
 
-  var link = document.createElement('a');
-  if (respStatus === 200) {
-    var response = JSON.parse(xhr.responseText);
-    if (response.success) {
-      link.textContent = response.files[0].url.replace(/.*?:\/\//g, "");
-      link.href = response.files[0].url;
+    percentIndicator.style.visibility = "hidden";
+    bar.style.visibility = "hidden";
+    row.removeChild(bar);
+    row.removeChild(percentIndicator);
+    var respStatus = xhr.status;
+
+    var url = document.createElement('span');
+    url.className = 'file-url';
+    row.appendChild(url);
+
+    var link = document.createElement('a');
+    if (respStatus === 200) {
+      var response = JSON.parse(xhr.responseText);
+      if (response.success) {
+        link.textContent = response.files[0].url.replace(/.*?:\/\//g, "");
+        link.href = response.files[0].url;
+        url.appendChild(link);
+      } else {
+        bar.innerHTML = 'Error: ' + response.reason;
+      }
+    } else if (respStatus === 413) {
+      link.textContent = 'File Too big!';
       url.appendChild(link);
     } else {
-      bar.innerHTML = 'Error: ' + response.reason;
+      link.textContent = 'Server error!';
+      url.appendChild(link);
     }
-  } else if (respStatus === 413) {
-    link.textContent = 'File Too big!';
-    url.appendChild(link);
-  } else {
-    link.textContent = 'Server error!';
-    url.appendChild(link);
   }
-}
 
-function uploadFile(file, row) {
-  var bar = row.querySelector('.file-progress');
-  var percentIndicator = row.querySelector('.progress-percent');
-  var xhr = new XMLHttpRequest();
-  xhr.open('POST', '/upload.php');
+  function uploadFile(file, row) {
+    var bar = row.querySelector('.file-progress');
+    var percentIndicator = row.querySelector('.progress-percent');
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/upload.php');
 
-  xhr["row"] = row;
-  xhr["bar"] = bar;
-  xhr["percent"] = percentIndicator;
-  xhr.upload["bar"] = bar;
-  xhr.upload["percent"] = percentIndicator;
+    xhr["row"] = row;
+    xhr["bar"] = bar;
+    xhr["percent"] = percentIndicator;
+    xhr.upload["bar"] = bar;
+    xhr.upload["percent"] = percentIndicator;
 
-  xhr.addEventListener('load', handleUploadComplete, false);
-  xhr.upload.onprogress = handleUploadProgress;
+    xhr.addEventListener('load', handleUploadComplete, false);
+    xhr.upload.onprogress = handleUploadProgress;
 
-  var form = new FormData();
-  form.append('files[]', file);
-  xhr.send(form);
-}
-
-function stopDefaultEvent(evt) {
-  evt.stopPropagation();
-  evt.preventDefault();
-}
-
-function handleDrag(state, element, evt) {
-  stopDefaultEvent(evt);
-  if (state.dragCount == 1) {
-    element.textContent = "Drop it here~";
+    var form = new FormData();
+    form.append('files[]', file);
+    xhr.send(form);
   }
-  state.dragCount += 1;
-}
 
-function handleDragAway(state, element, evt) {
-  stopDefaultEvent(evt);
-  state.dragCount -= 1;
-  if (state.dragCount == 0) {
-    element.textContent = "Select or drop file(s)";
+  function stopDefaultEvent(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
   }
-}
 
-function handleDragDrop(state, element, evt) {
-  stopDefaultEvent(evt);
-  handleDragAway(state, element, evt);
-  var len = evt.dataTransfer.files.length;
-  for (var i = 0; i < len; i++) {
-    var file = evt.dataTransfer.files[i];
-    var row = addRow(file);
-    uploadFile(file, row);
+  function handleDrag(state, element, evt) {
+    stopDefaultEvent(evt);
+    if (state.dragCount == 1) {
+      element.textContent = "Drop it here~";
+    }
+    state.dragCount += 1;
   }
-}
 
-function uploadFiles(evt) {
-  var len = evt.target.files.length;
-  for (var i = 0; i < len; i++) {
-    var file = evt.target.files[i];
-    var row = addRow(file);
-    uploadFile(file, row);
+  function handleDragAway(state, element, evt) {
+    stopDefaultEvent(evt);
+    state.dragCount -= 1;
+    if (state.dragCount == 0) {
+      element.textContent = "Select or drop file(s)";
+    }
   }
-}
 
-function selectFiles(target, evt) {
-  stopDefaultEvent(evt);
-  target.click();
-}
+  function handleDragDrop(state, element, evt) {
+    stopDefaultEvent(evt);
+    handleDragAway(state, element, evt);
+    var len = evt.dataTransfer.files.length;
+    for (var i = 0; i < len; i++) {
+      var file = evt.dataTransfer.files[i];
+      var row = addRow(file);
+      uploadFile(file, row);
+    }
+  }
 
-var state = { dragCount: 0 };
-var uploadButton = document.getElementById('upload-btn');
-window.addEventListener('dragenter', handleDrag.bind(this, state, uploadButton), false);
-window.addEventListener('dragleave', handleDragAway.bind(this, state, uploadButton), false);
-window.addEventListener('drop', handleDragAway.bind(this, state, uploadButton), false);
-window.addEventListener('dragover', stopDefaultEvent, false);
+  function uploadFiles(evt) {
+    var len = evt.target.files.length;
+    for (var i = 0; i < len; i++) {
+      var file = evt.target.files[i];
+      var row = addRow(file);
+      uploadFile(file, row);
+    }
+  }
 
-var uploadInput = document.getElementById('upload-input');
-uploadInput.addEventListener('change', uploadFiles);
-uploadButton.addEventListener('click', selectFiles.bind(this, uploadInput));
-uploadButton.addEventListener('drop', handleDragDrop.bind(this, state, uploadButton), false);
-document.getElementById('upload-form').classList.add('js');
+  function selectFiles(target, evt) {
+    stopDefaultEvent(evt);
+    target.click();
+  }
+
+  var state = { dragCount: 0 };
+  var uploadButton = document.getElementById('upload-btn');
+  window.addEventListener('dragenter', handleDrag.bind(this, state, uploadButton), false);
+  window.addEventListener('dragleave', handleDragAway.bind(this, state, uploadButton), false);
+  window.addEventListener('drop', handleDragAway.bind(this, state, uploadButton), false);
+  window.addEventListener('dragover', stopDefaultEvent, false);
+
+  var uploadInput = document.getElementById('upload-input');
+  uploadInput.addEventListener('change', uploadFiles);
+  uploadButton.addEventListener('click', selectFiles.bind(this, uploadInput));
+  uploadButton.addEventListener('drop', handleDragDrop.bind(this, state, uploadButton), false);
+  document.getElementById('upload-form').classList.add('js');
+});
