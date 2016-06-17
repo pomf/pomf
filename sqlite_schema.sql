41c71e18f61e8e0e6eca553c9d9e36277713f27c
akpu1 <akpu1@inventati.org>
2016-06-18 00:53:35 +1000

    Fixed misplaced comma

--- sqlite_schema.sql
+++ sqlite_schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE `files` (
 ,  `filename` varchar(30) default NULL
 ,  `size` integer  default NULL
 ,  `date` date default NULL
-,  `expire` date default NULL, -- Deprecated since version 2.1.0
+,  `expire` date default NULL
 ,  `delid` char(40) default NULL
 ,  `user` integer  default '0'
 ,  `dir` integer default '0'
